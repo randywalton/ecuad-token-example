01b818d65d43c0d7dd3372c9bc32878f86f23246
Randy <randy.l.walton@gmail.com>
2022-10-20 15:56:17 -0700

    updated styles, added more cards and labels

--- src/js/components/CardCss.js
+++ src/js/components/CardCss.js
@@ -4,11 +4,21 @@ Sheet.replaceSync(
     `
         p, h2 {
             margin:0px;
-            padding:0px
+            padding:0px;
+            line-height: 1.3;
         }
 
         h2 {
+            font-family: var(--global-typeface-headline-family);
+            font-size: var(--global-typeface-headline-size);
+            font-weight: 300;
             margin-bottom: var(--global-spacing-scale-small);
+        }
+
+        p {
+            font-family: var(--global-typeface-copy-family);
+            font-weight: 400;
+            font-size: var(--global-typeface-copy-size);
         }
 
         h2.light, p.light{
