7bbcbe3a5d6223df2ed81f5a034b072c18edbfa6
Randy <randy.l.walton@gmail.com>
2023-11-01 17:17:42 -0700

    updating

--- src/js/index.js
+++ src/js/index.js
@@ -47,4 +47,56 @@ document.addEventListener('DOMContentLoaded', (e) => {
     });
 
 
+
+    const options = {
+        element: ':root',
+        pretty: true
+    }
+
+
+    function testCSSVars() {
+        //get the token data
+        fetch('../data/tokens-v1.json')
+            .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
+            .then(data => {
+                //send the data to be sorted
+                console.log(data);
+                console.log(data.global);
+                console.log(data.global.refscale.primary);
+                console.log(Object.keys(data.global.refscale.primary.primary10).map(v => ({[v]: {...data[v]}})));
+                console.log(jsonToCssVariables(data.global.refscale.primary, options));
+            });
+    }
+    testCSSVars();
+
+   
+
+    function jsonToCssVariables(json, options) {
+        options = options || {}
+      
+        const offset = options.offset === undefined ? 0 : options.offset
+      
+        let count = 0
+        let output = `${options.element ? options.element : ':host'} {${options.pretty ? '\n' : ''}`
+      
+        for (let key in json) {
+          if (count >= offset) {
+            let value = json[key].value
+      
+            if (!isNaN(value) && value !== 0) {
+              value += options.unit === undefined ? 'px' : options.unit
+            }
+      
+            output += `${options.pretty ? '\t' : ''}--${key}: ${value};${options.pretty ? '\n' : ''}`
+          }
+      
+          count++
+        }
+      
+        output += '}'
+      
+        return output
+      }
+
+
 });
